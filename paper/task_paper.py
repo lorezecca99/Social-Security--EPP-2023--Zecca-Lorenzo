3a44b413ac9812917f941b42a099a08f13603d97
lorezecca99 <s78lzecc@uni-bonn.de>
2023-03-30 04:03:10 +0200

    Latex
    - Insert final results in the table
    - Latex file: abstract, results
    - Import table in latex file
    - Create task_paper

---
+++ paper/task_paper.py
@@ -0,0 +1,34 @@
+"""Task to compile 
+a LaTeX document and copy it to a specified 
+directory by using the pytask and pytask_latex modules."""
+
+import shutil
+
+import pytask
+from pytask_latex import compilation_steps as cs
+from proj_ss.config import BLD, PAPER_DIR
+
+documents=["proj_ss"]
+
+for document in documents:
+
+    @pytask.mark.latex(
+        script=PAPER_DIR / f"{document}.tex",
+        document=BLD / "latex" / f"{document}.pdf",
+        compilation_steps=cs.latexmk(
+            options=("--pdf", "--interaction=nonstopmode", "--synctex=1", "--cd"),
+        ),
+    )
+    @pytask.mark.task(id=document)
+    def task_compile_document():
+        """Compile the document specified in the latex decorator."""
+
+    kwargs = {
+        "depends_on": BLD / "latex" / f"{document}.pdf",
+        "produces": BLD.parent.resolve() / f"{document}.pdf",
+    }
+
+    @pytask.mark.task(id=document, kwargs=kwargs)
+    def task_copy_to_root(depends_on, produces):
+        """Copy a document to the root directory for easier retrieval."""
+        shutil.copy(depends_on, produces)
